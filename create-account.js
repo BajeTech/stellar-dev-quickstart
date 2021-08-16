a313077a0a17f85f3be9371b3f1eeef0897488f6
Owan Hunte <owanh@hotmail.com>
2021-08-15 23:20:13 -0400

    style: prettier format on create-account.js

--- create-account.js
+++ create-account.js
@@ -8,18 +8,14 @@ const pair = StellarSdk.Keypair.random();
 console.log(`Public Key: ${pair.publicKey()}`);
 console.log(`Secret Key: ${pair.secret()}`);
 
-// The SDK does not have tools for creating and funding test accounts, so you'll have to
+// The SDK does not have tools for creating test accounts, so you'll have to
 // make your own HTTP request.
 (async function createTestAccount() {
-  console.log(
-    "Funding a new account on the test network (takes a few seconds)…"
-  );
+  console.log("Funding a new account on the test network (takes a few seconds)…");
 
   try {
     const response = await fetch(
-      `https://friendbot.stellar.org?addr=${encodeURIComponent(
-        pair.publicKey(),
-      )}`,
+      `https://friendbot.stellar.org?addr=${encodeURIComponent(pair.publicKey())}`
     );
     const responseJSON = await response.json();
     console.log("SUCCESS! You have a new account :)\n", responseJSON);
@@ -29,7 +25,7 @@ console.log(`Secret Key: ${pair.secret()}`);
 
   const server = new StellarSdk.Server("https://horizon-testnet.stellar.org");
 
-  // The JS SDK uses promises for most actions, such as retrieving an account
+  // The JS SDK uses promises for most actions, such as retrieving an account.
   const account = await server.loadAccount(pair.publicKey());
   console.log("\nBalances for account: " + pair.publicKey());
   account.balances.forEach(function (balance) {
